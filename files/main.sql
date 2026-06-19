4a1557d666f71966e00ec0a782acc6f8c5df40db
azmanabdlh <azmanabdlh12@gmail.com>
2026-06-19 19:56:49 +0900

    update password-hash

--- files/main.sql
+++ files/main.sql
@@ -63,7 +63,7 @@ INSERT INTO users (email, password_hash)
 VALUES
 (
     'ayo@example.com',
-    '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy'
+    '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'
 );
 
 -- ======================================
